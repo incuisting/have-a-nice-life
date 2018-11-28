82ac449c16f1a38cf04d42d5a0b5bd0d7f987ccf
xuyanting <xuyanting9634@gmail.com>
2018-11-29 05:30:32 +0800

    change user

--- userInfo.js
+++ userInfo.js
@@ -8,22 +8,24 @@ module.exports = {
   data: [
     {
       openid: 'oKNukjhsMYqMEW6KMJZdmiciI1O8',
-      peopleNum: 4,
+      peopleNum: 6,
       url:
         'https://superapp.kiwa-tech.com/weexWeb/index.html?page=stores-numeral.web.js&moduleId=row-num&storeName=rowNum&source=weixin&weixin_user_id=oKNukjhsMYqMEW6KMJZdmiciI1O8&realname=nNQweixin%E5%BE%90%E7%84%B1%E6%8C%BA&mobile=&cuspoint=&gender=m'
-    },
-    {
-      openid: 'oKNukjsp8tqX6p9ilw-ZiDy-FJ0I',
-      peopleNum: 4,
-      url:
-        'https://superapp.kiwa-tech.com/weexWeb/index.html?page=stores-numeral.web.js&moduleId=row-num&storeName=rowNum&source=weixin&weixin_user_id=oKNukjsp8tqX6p9ilw-ZiDy-FJ0I&realname=jfcweixin%E9%BB%84%E6%A2%B5%E5%8D%9A&mobile=&cuspoint=&gender=m'
-    },
-    {
-      openid: 'oKNukjuDiWDpMdbMw9tntWugKDTA',
-      peopleNum: 4,
-      url:
-        'https://superapp.kiwa-tech.com/weexWeb/index.html?page=stores-numeral.web.js&moduleId=row-num&storeName=rowNum&source=weixin&weixin_user_id=oKNukjuDiWDpMdbMw9tntWugKDTA&realname=EOWweixin%E4%BC%8D%E6%95%8F%E8%B1%AA&mobile=&cuspoint=&gender=m'
     }
+    //xl
+    // {
+    //   openid: 'oKNukjsp8tqX6p9ilw-ZiDy-FJ0I',
+    //   peopleNum: 4,
+    //   url:
+    //     'https://superapp.kiwa-tech.com/weexWeb/index.html?page=stores-numeral.web.js&moduleId=row-num&storeName=rowNum&source=weixin&weixin_user_id=oKNukjsp8tqX6p9ilw-ZiDy-FJ0I&realname=jfcweixin%E9%BB%84%E6%A2%B5%E5%8D%9A&mobile=&cuspoint=&gender=m'
+    // },
+    //xw
+    // {
+    //   openid: 'oKNukjuDiWDpMdbMw9tntWugKDTA',
+    //   peopleNum: 4,
+    //   url:
+    //     'https://superapp.kiwa-tech.com/weexWeb/index.html?page=stores-numeral.web.js&moduleId=row-num&storeName=rowNum&source=weixin&weixin_user_id=oKNukjuDiWDpMdbMw9tntWugKDTA&realname=EOWweixin%E4%BC%8D%E6%95%8F%E8%B1%AA&mobile=&cuspoint=&gender=m'
+    // }
     // {
     //   openid: 'oKNukjg9fCn-IgdydB48_TB3TowE',
     //   peopleNum: 6
