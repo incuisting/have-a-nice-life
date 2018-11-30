3867715faf234cca2f0e878172d7fcfc7fe8a0ba
xuyanting <xuyanting9634@gmail.com>
2018-11-30 23:56:28 +0800

    修改定时任务配置

--- cornd.js
+++ cornd.js
@@ -1,6 +1,11 @@
 const { CronJob } = require('cron')
-const { main } = require('./index')
-const job = new CronJob('0 0 8,16 * * *', function() {
-  main()
-})
-job.start()
+const { main, getAuthData } = require('./index')
+const job = [
+  new CronJob('0,1,2,3,4,5,6,7,8,9,10 0 8,16 * * *', function() {
+    main()
+  }),
+  new CronJob('0 50 7,15 * * *', function() {
+    getAuthData()
+  })
+]
+job.forEach(item => item.start())
