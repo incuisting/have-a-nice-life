808b79ca0b7776fa2c76f59fad66e3bf71ac333d
xuyanting <xuyanting9634@gmail.com>
2018-12-01 00:03:29 +0800

    index 改造

--- index.js
+++ index.js
@@ -2,47 +2,17 @@ const { fetch, initSubmitQueue, getAuth } = require('./utils')
 const { data, storeId } = require('./userInfo')
 const { headerBuilder } = require('./requestInfo')
 const { logger } = require('./log')
-
-function filterSuccess(resultArray, promiseArray) {
-  let res = []
-  resultArray.forEach((item, idx) => {
-    if (!item.body.success) {
-      res.push(promiseArray[idx])
-    }
-  })
-  return res
-}
-function isContinue(reFetch1, reFetch2) {
-  if (reFetch1.length === 0) {
-    return false
-  }
-  if (reFetch2.length === 0) {
-    return false
-  }
-  return true
-}
+//TODO
+//直接采用文件中读取auth的方式
+//包装promise 之后直接发送
 async function main() {
   let allQueue1 = initSubmitQueue(data, '1')
   let allQueue2 = initSubmitQueue(data, '2')
   let result1 = await Promise.all(allQueue1)
   let result2 = await Promise.all(allQueue2)
-  let reFetch1 = filterSuccess(result1, allQueue1)
-  let reFetch2 = filterSuccess(result2, allQueue2)
-  let queueStatus = isContinue(reFetch1, reFetch2)
   logger.info(result1, result2)
-  // console.log(result1, result2)
-  while (queueStatus) {
-    result1 = await Promise.all(reFetch1)
-    result2 = await Promise.all(reFetch2)
-    reFetch1 = filterSuccess(result1, allQueue1)
-    reFetch2 = filterSuccess(result2, allQueue2)
-    queueStatus = isContinue(reFetch1, reFetch2)
-    logger.info(result1, result2)
-    // console.log(result1, result2)
-  }
 }
 
-main()
-// module.exports = {
-//   main
-// }
+module.exports = {
+  main
+}
