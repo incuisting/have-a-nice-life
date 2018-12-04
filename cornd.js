076d6a1218dd8da748fdca234423075702e1bf93
xuyanting <393146040@qq.com>
2018-12-04 08:53:57 +0800

    modify crontab date info

--- cornd.js
+++ cornd.js
@@ -3,7 +3,7 @@ const { main } = require('./index')
 const { getAuthData } = require('./getAuth')
 
 const job = [
-  new CronJob(`* 0 8,16 * * *`, function() {
+  new CronJob(`* 0,1 8,16 * * *`, function() {
     main()
   }),
   new CronJob('0 50 7,15 * * *', function() {
