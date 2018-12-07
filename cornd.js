c046c6b06dd856f9a5b5e67daf13a6bce37f97cb
ubuntu <ubuntu@localhost.localdomain>
2018-12-07 16:39:36 +0800

    before debug

--- cornd.js
+++ cornd.js
@@ -3,11 +3,12 @@ const { main } = require('./index')
 const { getAuthData } = require('./getAuth')
 
 const job = [
-  new CronJob(`* 0,1 8,16 * * *`, function() {
+  new CronJob('* 0 16 * * *', function() {
     main()
   }),
   new CronJob('0 50 7,15 * * *', function() {
     getAuthData()
+    console.log('time', new Date().toLocaleString())
   })
 ]
 job.forEach(item => item.start())
